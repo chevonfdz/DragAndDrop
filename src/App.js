b27dc184ac7293562601572ab86f82e65ace7893
chevonfdz <86297885+chevonfdz@users.noreply.github.com>
2022-12-12 11:27:58 +0530

    Updated Json formatter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import "reactjs-popup/dist/index.css";
-import MediatorButton from "./Componets/MediatorButton";
-import LogMediator from "./Componets/LogMediator";
-import CallMediator from "./Componets/CallMediator";
-import DropMediator from "./Componets/DropMediator";
-import PropertyMediator from "./Componets/PropertyMediator";
+import MediatorButton from "./Components/MediatorButton";
+import LogMediator from "./Components/LogMediator";
+import CallMediator from "./Components/CallMediator";
+import DropMediator from "./Components/DropMediator";
+import PropertyMediator from "./Components/PropertyMediator";
 import { useState } from "react";
-import LineTo from 'react-lineto';
 
 function App() {
+  const JSONPretty = require("react-json-pretty");
+  const theme = require("react-json-pretty/dist/monikai");
   const [apiState, setApiState] = useState({
     api: {
       resource: {
@@ -20,57 +21,88 @@ function App() {
   });
 
   return (
-    <div id="ContentArea">
-      <MediatorButton
-        onLogClick={() => {
-          const nextApiInSequence = { ...apiState.api.resource.inSequence, log: "" };
-          const nextResource = {
-            ...apiState.api.resource,
-            inSequence: nextApiInSequence,
-          };
-          const nextApi = { ...apiState.api, resource: nextResource };
-          const nextApiState = { ...apiState, api: nextApi };
-          setApiState(nextApiState);
+    <div className="ContentArea">
+      <div className="DraggingArea">
+        {/* <button className="codeBtn">
+          code
+        </button> */}
 
-        }}
-      />
+        <MediatorButton
+          onLogClick={() => {
+            const nextApiInSequence = {
+              ...apiState.api.resource.inSequence,
+              log: "",
+            };
+            const nextResource = {
+              ...apiState.api.resource,
+              inSequence: nextApiInSequence,
+            };
+            const nextApi = { ...apiState.api, resource: nextResource };
+            const nextApiState = { ...apiState, api: nextApi };
+            setApiState(nextApiState);
+          }}
+          onCallClick={() => {
+            const nextApiInSequence = {
+              ...apiState.api.resource.inSequence,
+              call: "",
+            };
+            const nextResource = {
+              ...apiState.api.resource,
+              inSequence: nextApiInSequence,
+            };
+            const nextApi = { ...apiState.api, resource: nextResource };
+            const nextApiState = { ...apiState, api: nextApi };
+            setApiState(nextApiState);
+          }}
+          onDropClick={() => {
+            const nextApiInSequence = {
+              ...apiState.api.resource.inSequence,
+              drop: "",
+            };
+            const nextResource = {
+              ...apiState.api.resource,
+              inSequence: nextApiInSequence,
+            };
+            const nextApi = { ...apiState.api, resource: nextResource };
+            const nextApiState = { ...apiState, api: nextApi };
+            setApiState(nextApiState);
+          }}
+          onPropertyClick={() => {
+            const nextApiInSequence = {
+              ...apiState.api.resource.inSequence,
+              property: "",
+            };
+            const nextResource = {
+              ...apiState.api.resource,
+              inSequence: nextApiInSequence,
+            };
+            const nextApi = { ...apiState.api, resource: nextResource };
+            const nextApiState = { ...apiState, api: nextApi };
+            setApiState(nextApiState);
+          }}
+        />
 
-      {
-        (typeof apiState.api.resource.inSequence.log != 'undefined') ? <LogMediator/> : 
-        (typeof apiState.api.resource.inSequence.call != 'undefined') ? <CallMediator/> : 
-        (typeof apiState.api.resource.inSequence.drop != 'undefined') ? <DropMediator/> : 
-        (typeof apiState.api.resource.inSequence.property != 'undefined') ? <PropertyMediator/> : null
+        {typeof apiState.api.resource.inSequence.log != "undefined" ? (
+          <LogMediator />
+        ) : typeof apiState.api.resource.inSequence.call != "undefined" ? (
+          <CallMediator />
+        ) : typeof apiState.api.resource.inSequence.drop != "undefined" ? (
+          <DropMediator />
+        ) : typeof apiState.api.resource.inSequence.property != "undefined" ? (
+          <PropertyMediator />
+        ) : null}
+      </div>
 
-        // getUiElement('log')
-        // () => {
-        //   console.log('log');
-        //   for (var key in apiState.api.resource.inSequence) {
-        //     if (apiState.api.resource.inSequence.hasOwnProperty(key)) {
-        //       console.log(key + " -> " + apiState.api.resource.inSequence[key]);
-        //       switch (key) {
-        //         case 'log':
-        //           return <MediatorButton/>;
-        //           break;
-        //       }
-        //       // getUiElement(key);
-        //     }
-        //   }
-        // }
-
-      }
-
-      <div className="JsonText">{JSON.stringify(apiState)}</div>
+      <div className="CodeTextArea">
+        <JSONPretty
+          className="json-display"
+          id="json-pretty"
+          data={JSON.stringify(apiState)}
+          theme={theme}
+        ></JSONPretty>
+      </div>
     </div>
   );
-}
-
-function getUiElement(type) {
-  switch (type) {
-    case 'log':
-      return <MediatorButton/>;
-      break;
-  }
-
 }
 
 export default App;
